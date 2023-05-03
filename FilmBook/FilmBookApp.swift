63ec6465cad1fa8a3d4c88b3232e2ee60729453a
Taner Celik <tanercelik2001@gmail.com>
2023-05-03 19:13:23 +0300

    Initial Commit

---
+++ FilmBook/FilmBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmBookApp.swift
+//  FilmBook
+//
+//  Created by Taner Çelik on 3.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FilmBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
