02bdb7725ac60680dc6e4301b74278e51925b7bb
Taner Celik <tanercelik2001@gmail.com>
2023-05-07 17:28:21 +0300

    FilmDetay Service

--- FilmBook/Services/DownloaderClient.swift
+++ FilmBook/Services/DownloaderClient.swift
@@ -10,7 +10,7 @@ import Foundation
 class DownloaderClient{
     
     func filmleriIndır(search: String, completion : @escaping (Result<[Film]?, DownloaderError>) -> Void ) {
-       guard let url = URL(string: "https://www.omdbapi.com/?s=\(search)&apikey=6d6ea303") else{
+        guard let url = URL(string: "https://www.omdbapi.com/?s=\(search)&apikey=6d6ea303") else{
             return completion(.failure(.yanlisUrl))
         }
         
@@ -27,9 +27,35 @@ class DownloaderClient{
         } .resume()
     }
     
-    enum DownloaderError : Error {
-        case yanlisUrl
-        case veriGelmedi
-        case veriIslenemedi
+    
+    
+    // Tek bir filmi indirmek için gereken downloader client
+    
+    func filmDetayiIndir (imdbId: String, completion : @escaping (Result<FilmDetay, DownloaderError>) -> Void ) {
+        
+        guard let url = URL(string: "https://www.omdbapi.com/?i=\(imdbId)&apikey=6d6ea303") else{
+            return completion(.failure(.yanlisUrl))
+        }
+        
+        URLSession.shared.dataTask(with: url) {  (data, response, error) in
+            guard let data = data , error == nil else {
+                return completion(.failure(.veriGelmedi))
+            }
+            
+            guard let gelenFilmDetayi = try? JSONDecoder().decode(FilmDetay.self, from: data)
+            else {
+                return completion(.failure(.veriIslenemedi))
+            }
+            completion(.success(gelenFilmDetayi))
+        }.resume()
+        
     }
+    
+}
+
+    
+enum DownloaderError : Error {
+    case yanlisUrl
+    case veriGelmedi
+    case veriIslenemedi
 }
