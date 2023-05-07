1203b48c3f94fc6bcea4baf1e378b071ca36f58e
Taner Celik <tanercelik2001@gmail.com>
2023-05-07 12:16:19 +0300

    FilmListeView Designed

--- FilmBook/View/FilmListeView.swift
+++ FilmBook/View/FilmListeView.swift
@@ -21,7 +21,21 @@ struct FilmListeView: View {
     var body: some View {
         List(filmListeViewModel.filmler, id: \.imdbId) {
             film in
-            Text(film.title)
+            HStack{
+                Image("placeholder2")
+                    .resizable()
+                    .frame(width: 100, height: 150)
+                
+                VStack (alignment: .leading){
+                    Text(film.title)
+                        .font(.title3).bold()
+                    
+                    
+                    Text(film.year)
+                        .font(.subheadline)
+                    
+                }
+            }
         }
     }
 }
